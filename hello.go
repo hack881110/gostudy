85fa989eae16d3ede3ae5dc393df4ccf0a9b755c
wanghao <122344140@qq.com>
2016-10-21 13:58:18 +0800

    add select study

--- hello.go
+++ hello.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+)
 
-func main(){
-    fmt.Println("Hello ,world!")
+func main() {
+	fmt.Print("Go runs on ")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		// freebsd, openbsd,
+		// plan9, windows...
+		fmt.Printf("%s.", os)
+	}
 }
--- select.go
+++ select.go
@@ -1,10 +1,13 @@
 package main
 
 import "fmt"
+import "runtime"
 
 func fibonacci(c, quit chan int) {
 	x, y := 0, 1
+	fmt.Print("hello\t")
 	for {
+		fmt.Print("send\t")
 		select {
 		case c <- x:
 			x, y = y, x+y
@@ -19,10 +22,16 @@ func main() {
 	c := make(chan int)
 	quit := make(chan int)
 	go func() {
+		fmt.Print("world\t")
 		for i := 0; i < 10; i++ {
+			fmt.Print("before\t")
 			fmt.Println(<-c)
+			fmt.Print("out\t", i)
 		}
 		quit <- 0
 	}()
+
+	num := runtime.NumCPU()
+	fmt.Print(num)
 	fibonacci(c, quit)
 }
